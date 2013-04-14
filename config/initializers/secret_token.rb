a75f94bc21434700d67ef6a39784946a6dca0e70
rem_cyb <rem_cyb@yahoo.fr>
2013-04-14 22:47:08 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChevalApp::Application.config.secret_token = 'd07e85e3b013d3b0a32ce69319d27d67f2a7a3235d760e4e94db676bab257c27e7694193fa4dfedae7ceb96c646b6e901f9658b64fb829fb503dac87b88f62b4'
